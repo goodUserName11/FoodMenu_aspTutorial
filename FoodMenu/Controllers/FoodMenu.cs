6f5fabdfc311beb5e93c86ba858c3e9f0b11bd3b
Kireev Konstantin <kireevkonstantin11@gmai.com>
2024-07-18 18:24:33 +0400

    Added FoodMenu controller and menu index, details pages

---
+++ FoodMenu/Controllers/FoodMenu.cs
@@ -0,0 +1,35 @@
+﻿using FoodMenu.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Runtime.CompilerServices;
+
+namespace FoodMenu.Controllers
+{
+    public class FoodMenu : Controller
+    {
+        private readonly FoodMenuContext _context;
+
+        public FoodMenu(FoodMenuContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Dishes.ToListAsync());
+        }
+
+        public async Task<IActionResult> Details(int? id)
+        {
+            var dish = await _context.Dishes
+                .Include(di => di.DishIngridients)
+                .ThenInclude(i => i.Ingredient)
+                .FirstOrDefaultAsync(x => x.Id == id);
+
+            if(dish is null)
+                return NotFound();
+
+            return View(dish);
+        }
+    }
+}
